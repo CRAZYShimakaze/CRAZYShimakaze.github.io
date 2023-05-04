2e491614108625fc7289b694f0cde1c5176ae2e5
CRAZYShimakaze <674015283@qq.com>
2023-05-05 01:47:36 +0800

    update md5

--- cal_md5.py
+++ cal_md5.py
@@ -8,10 +8,10 @@ def get_hash(path):
     hash_list = {}
     data = os.listdir(path)
     for item in data:
-        if item != 'md5':
+        if item != 'md5.json':
             with open(f'{path}/{item}', 'rb') as img:
                 hash_list[item.replace('.jpg', '').replace('.png', '')] = hashlib.md5(img.read()).hexdigest()
-    path_ = Path(f'{path}/md5')
+    path_ = Path(f'{path}/md5.json')
     json.dump(hash_list, path_.open('w', encoding='utf-8'), ensure_ascii=False, indent=4)
 
 
@@ -20,8 +20,8 @@ get_hash('genshin/role_info')
 get_hash('genshin/role_break')
 get_hash('genshin/role_guide')
 get_hash('genshin/weapon_info')
-#get_hash('starrail/common_guide')
-#get_hash('starrail/role_info')
-#get_hash('starrail/role_break')
+# get_hash('starrail/common_guide')
+# get_hash('starrail/role_info')
+# get_hash('starrail/role_break')
 get_hash('starrail/role_guide')
 get_hash('starrail/weapon_info')
