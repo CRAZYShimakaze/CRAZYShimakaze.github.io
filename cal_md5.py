eabfd2437145a93ee5909ec4f61226ecaf12d426
crazy <674015283@qq.com>
2024-09-11 00:28:47 +0800

    更新攻略

--- cal_md5.py
+++ cal_md5.py
@@ -15,11 +15,33 @@ def get_hash(path):
     json.dump(hash_list, path_.open('w', encoding='utf-8'), ensure_ascii=False, indent=4)
 
 
+def get_alias(path, alias):
+    if 'common_guide' in path:
+        return
+    for item in os.listdir(path):
+        if 'json' in item:
+            continue
+        name = item.replace('.jpg', '').replace('.png', '')
+        if name not in alias['角色'] and 'role' in path:
+            alias['角色'][name] = []
+        if name not in alias['武器'] and 'weapon' in path:
+            alias['武器'][name] = [name]
+
+
 genshin = os.listdir('genshin')
+path_ = Path(f'genshin/alias.json')
+alias = json.load(path_.open('r', encoding='utf-8'))
 for item in genshin:
     if os.path.isdir('genshin/' + item):
+        get_alias('genshin/' + item, alias)
         get_hash('genshin/' + item)
+json.dump(alias, path_.open('w', encoding='utf-8'), ensure_ascii=False, indent=2)
+
 starrail = os.listdir('starrail')
+# path_ = Path(f'starrail/alias.json')
+# alias = json.load(path_.open('r', encoding='utf-8'))
 for item in starrail:
     if os.path.isdir('starrail/' + item):
+        # get_alias('starrail/' + item, alias)
         get_hash('starrail/' + item)
+# json.dump(alias, path_.open('w', encoding='utf-8'), ensure_ascii=False, indent=2)
